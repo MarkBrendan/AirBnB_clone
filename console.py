99deda026f54d86117a73c141ed23590ab87bd0c
MarkBrendan <markobinna65@gmail.com>
2024-05-19 00:38:54 +0100

    console

---
+++ console.py
@@ -0,0 +1,22 @@
+import cmd
+""" Command interpreter module"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """command processor"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        "Exit from the program with quit"
+        return True
+
+    def do_EOF(self, line):
+        "Exit from the program with ctrl+D"
+        return True
+
+    def emptyline(self):
+        """do nothing when an empty line is enter"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
